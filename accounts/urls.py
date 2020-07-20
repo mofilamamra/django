ec33cbac86c57cf45015f8f80be436201983c8ab
mofi <mofbiskra83@gmail.com>
2020-07-20 14:10:30 +0200

    message du commit2

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import signup
+from .views import signup, activate_email
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('activate/<uid>/<token>/', activate_email, name='activate_email'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,46 @@
-from django.shortcuts import render, redirect
-from .forms import SignUpForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http.response import HttpResponseBadRequest
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .forms import SignUpForm
+# , UserProfileForm
+from .utils import send_confirmation_email
+from .tokens import confirm_email_token_generator
+
+User = get_user_model()
 
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
+        #profile_form = UserProfileForm(request.POST)
+
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            user = form.save()
+            user.is_active = False
+
+           # profile = profile_form.save(commit=False)
+           # profile.user = user
+
+            user.save()
+            send_confirmation_email(request, user)
+
+            return render(request, 'registration/signup_success.html')
+
     else:
         form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form})
+       # profile_form = UserProfileForm()
+
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
+
+
+def activate_email(request, uid, token):
+    user = get_object_or_404(User, pk=uid)
+    if confirm_email_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+
+        return redirect('login')
+    else:
+        return HttpResponseBadRequest('Bad Token')
